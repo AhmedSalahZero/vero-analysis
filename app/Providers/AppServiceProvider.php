f7f764ac68cbf4557dd1a834e3b627420debb9e1
ahmed salah <asalahdev5@gmail.com>
2024-11-26 14:47:19 +0200

    fix main issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -47,7 +47,6 @@ class AppServiceProvider extends ServiceProvider
 
 		ini_set('max_execution_time', 6000); //300 seconds = 5 minutes
 
-	
 		\PhpOffice\PhpSpreadsheet\Shared\Font::setAutoSizeMethod(Font::AUTOSIZE_METHOD_EXACT);
 		require_once storage_path('dompdf/vendor/autoload.php');
 		require_once app_path('Helpers/HArr.php');
