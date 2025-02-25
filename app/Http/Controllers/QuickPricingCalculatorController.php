eacecf149a2c4a78957e9aa9335688c9698dff66
ahmed salah <asalahdev5@gmail.com>
2025-02-25 10:08:22 +0200

    sharing

--- app/Http/Controllers/QuickPricingCalculatorController.php
+++ app/Http/Controllers/QuickPricingCalculatorController.php
@@ -77,7 +77,7 @@ class QuickPricingCalculatorController extends Controller
 		if(!$sharingLink && $pricingPlanId != 0){
 			abort(404);
 		}
-		$pricingPlanId = is_null($sharingLink->shareable_id) ? $pricingPlanId :  $sharingLink->shareable_id;
+		$pricingPlanId = !$sharingLink ? $pricingPlanId :  $sharingLink->shareable_id;
         return view('admin.quick-pricing-calculator.create' , array_merge(
 			QuickPricingCalculator::getViewVars(),
 			['pricingPlanId'=>$pricingPlanId]
