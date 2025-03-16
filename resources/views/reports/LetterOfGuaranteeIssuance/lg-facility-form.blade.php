3fad407ace340e9e39f5e0a63e24bdee03602018
ahmed salah <asalahdev5@gmail.com>
2025-03-17 00:13:24 +0200

    fix lg issue

--- resources/views/reports/LetterOfGuaranteeIssuance/lg-facility-form.blade.php
+++ resources/views/reports/LetterOfGuaranteeIssuance/lg-facility-form.blade.php
@@ -334,10 +334,11 @@ use App\Models\LetterOfGuaranteeIssuance;
                                         <label> {{ __('LG Amount') }}
                                             @include('star')
                                         </label>
-                                        <div>
-                                            <input required value="{{ (isset($model) ? number_format($model->getLgAmount(),0) : 0) }}" class="only-greater-than-or-equal-zero-allowed only-smaller-than-or-equal-specific-number-allowed recalculate-cash-cover-amount-js recalculate-lg-commission-amount-js lg-amount-js" type="text" placeholder="{{ __('Lg Amount') }}">
-                                            <input type="hidden" value="{{ (isset($model) ? $model->getLgAmount() : 0) }}" name="lg_amount" class="only-greater-than-zero-allowed ">
+										                                        {{-- <x-form.input  :data-current-value="isset($model) ? $model->getLgAmount():0" :data-current-value="isset($model) ? $model->getLgAmount():0" :model="$model??null" :label="__('LG Amount')" :type="'text'" :placeholder="__('LG Amount')" :name="'lg_amount'" :class="'only-greater-than-or-equal-zero-allowed only-smaller-than-or-equal-specific-number-allowed recalculate-cash-cover-amount-js recalculate-lg-commission-amount-js lg-amount-js '"  :required="true"></x-form.input> --}}
 
+                                        <div>
+                                            <input data-current-value="{{ isset($model) ? $model->getLgAmount() : 0 }}" required value="{{ (isset($model) ? number_format($model->getLgAmount(),0) : 0) }}" class="form-control only-greater-than-or-equal-zero-allowed only-smaller-than-or-equal-specific-number-allowed recalculate-cash-cover-amount-js recalculate-lg-commission-amount-js lg-amount-js" type="text" placeholder="{{ __('Lg Amount') }}">
+                                            <input data-current-value="{{ isset($model) ? $model->getLgAmount() : 0 }}" type="hidden" value="{{ (isset($model) ? $model->getLgAmount() : 0) }}" name="lg_amount" class="only-greater-than-zero-allowed ">
                                         </div>
                                     </div>
 
