473032741343bd7237f6deceeb446d9caeddd840
ahmed salah <asalahdev5@gmail.com>
2025-04-24 01:57:54 +0200

    f

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1883,12 +1883,12 @@ function yearAndMonthInArray(string $date, array $dates)
 function array_sum_conditional($data, $dates, $incomeStatementStartDate, $incomeStatementDurationType)
 {
     $incomeStatementStartDate = Carbon::make($incomeStatementStartDate);
-	$incomeStatementDurationType='annually';
+	// $incomeStatementDurationType='annually';
     $total = 0;
     foreach ($data as $date => $value) {
 		if ($incomeStatementDurationType == 'annually') {
 			// dd('e',$dates[$date]);
-            if (yearInArray($dates[$date], $dates)) {
+            if (isset($dates[$date]) && yearInArray($dates[$date], $dates)) {
 				$total += $value;
             }
         } else {
