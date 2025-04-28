13037c21bc5af07870f57150e19d655dad3bbc60
ahmed salah <asalahdev5@gmail.com>
2025-04-28 13:49:38 +0300

    dd

--- app/Exports/ActualTemplateExport.php
+++ app/Exports/ActualTemplateExport.php
@@ -49,6 +49,7 @@ class ActualTemplateExport implements
 	
 		 $mainItemsWithSubItems = $this->incomeStatement->mainItems->where('has_sub_items',1)->filter(function(IncomeStatementItem $mainItem){
 			$subItemsNames = $mainItem->subItems()->wherePivot('sub_item_type', 'forecast')->wherePivot('financial_statement_able_id',$this->incomeStatement->id)->wherePivot('financial_statement_able_item_id',$mainItem->id)->orderBy('financial_statement_able_item_id')->get()->pluck('pivot.sub_item_name','pivot.id') ;
+			dd($subItemsNames);
 			return $mainItem->setRelation('sub_items_name',$subItemsNames);
 		}) ;
 		foreach($mainItemsWithSubItems as $mainItem )
