9d5e8eb71698c2e1baf96303b29ba92de3204324
ahmed salah <asalahdev5@gmail.com>
2025-05-07 12:17:00 +0300

    f

--- resources/views/client_view/sales_gathering/import.blade.php
+++ resources/views/client_view/sales_gathering/import.blade.php
@@ -15,7 +15,6 @@ elseif($modelName == 'LoanSchedule'){
 elseif($modelName =='ExpenseAnalysis'){
 	$redirectUrl = route('view.expense.analysis.dashboard',['company'=>$company->id]);
 }
-dd(Request('medium_term_loan_id'));
 $additionalArray = $modelName == 'LoanSchedule' ? ['medium_term_loan_id'=>Request('medium_term_loan_id')] : [];
 
 
