530f294ca10f7ec0ab8330ecdc195572eb5a8e07
Ahmed Salah <asalahdev5@gmail.com>
2025-10-30 19:10:25 +0300

    f

--- database/migrations/2025_10_30_174053_create_new_income_statements_table.php
+++ database/migrations/2025_10_30_174053_create_new_income_statements_table.php
@@ -13,21 +13,21 @@ class CreateNewIncomeStatementsTable extends Migration
      */
     public function up()
     {
-        Schema::connection(NON_BANKING_SERVICE_CONNECTION_NAME)->create('test_income_statements', function (Blueprint $table) {
-            $table->id();
-			$table->json('revenues');
-			$table->json('oda_interests');
-			$table->json('corporate_taxes');
-            $table->timestamps();
-        });
-		  Schema::connection(NON_BANKING_SERVICE_CONNECTION_NAME)->create('test_cashflow_statements', function (Blueprint $table) {
-            $table->id();
-			$table->json('cash_in');
-			$table->json('cash_out');
-			$table->json('oda_interests');
-			$table->json('corporate_taxes_payments');
-            $table->timestamps();
-        });
+        // Schema::connection(NON_BANKING_SERVICE_CONNECTION_NAME)->create('test_income_statements', function (Blueprint $table) {
+        //     $table->id();
+		// 	$table->json('revenues');
+		// 	$table->json('oda_interests');
+		// 	$table->json('corporate_taxes');
+        //     $table->timestamps();
+        // });
+		//   Schema::connection(NON_BANKING_SERVICE_CONNECTION_NAME)->create('test_cashflow_statements', function (Blueprint $table) {
+        //     $table->id();
+		// 	$table->json('cash_in');
+		// 	$table->json('cash_out');
+		// 	$table->json('oda_interests');
+		// 	$table->json('corporate_taxes_payments');
+        //     $table->timestamps();
+        // });
     }
 
     /**
