d1bd9efa5be3cb6b9ce3dbc701bd3f86616b5d78
Ahmed Salah <asalahdev5@gmail.com>
2025-11-05 17:25:47 +0200

    format number

--- resources/views/client_view/homePage.blade.php
+++ resources/views/client_view/homePage.blade.php
@@ -207,7 +207,9 @@
                                         @endif
 {{-- {{ dd(auth()->user()->id) }} --}}
 
-                                        @if(auth()->user()->id == 1 || auth()->user()->id == 94)
+                                        @if(auth()->user()->id == 1 )
+										
+										@if(auth()->user()->id == 94)
                                         <div class="card">
                                             <div class="card-header" id="nonbankingserviceSection">
                                                 <div class="card-title collapsed" data-toggle="collapse" data-target="#collapseNonbankingserviceSection" aria-expanded="true" aria-controls="collapseOne4">
@@ -224,6 +226,7 @@
                                                 </div>
                                             </div>
                                         </div>
+										@endif
 										
 										 <div class="card">
                                             <div class="card-header" id="financialplanning">
