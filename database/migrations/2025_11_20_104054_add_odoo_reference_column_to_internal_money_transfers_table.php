054ac14738f4d830120b3ddaaa70b3dd545a84e0
Ahmed Salah <asalahdev5@gmail.com>
2025-11-20 17:19:19 +0200

    fix issues

--- database/migrations/2025_11_20_104054_add_odoo_reference_column_to_internal_money_transfers_table.php
+++ database/migrations/2025_11_20_104054_add_odoo_reference_column_to_internal_money_transfers_table.php
@@ -14,7 +14,7 @@ class AddOdooReferenceColumnToInternalMoneyTransfersTable extends Migration
     public function up()
     {
         Schema::table('internal_money_transfers', function (Blueprint $table) {
-            $table->boolean('synced_with_odoo')->after('id')->nullable();
+            // $table->boolean('synced_with_odoo')->after('id')->nullable();
             // $table->string('odoo_error_message')->after('id')->nullable();
             // $table->string('inbound_odoo_reference')->after('id')->nullable();
             // $table->string('outbound_odoo_reference')->after('id')->nullable();
