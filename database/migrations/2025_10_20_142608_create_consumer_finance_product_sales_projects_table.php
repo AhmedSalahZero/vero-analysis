7fae03f0fcd3b7287896d0e8356727dd921c9c11
Ahmed Salah <asalahdev5@gmail.com>
2025-12-15 09:02:21 +0200

    fix column not found

--- database/migrations/2025_10_20_142608_create_consumer_finance_product_sales_projects_table.php
+++ database/migrations/2025_10_20_142608_create_consumer_finance_product_sales_projects_table.php
@@ -14,20 +14,20 @@ class CreateConsumerfinanceProductSalesProjectsTable extends Migration
     public function up()
     {
 		
-        // Schema::connection(NON_BANKING_SERVICE_CONNECTION_NAME)->create('consumerfinance_product_sales_projects', function (Blueprint $table) {
-        //     $table->id();
-		// 	$table->unsignedBigInteger('consumerfinance_product_id');
-		// 	$table->unsignedBigInteger('tenor')->default(0);
-		// 	$table->decimal('avg_amount',14,2)->default(0);
-		// 	$table->json('monthly_amounts')->nullable();
-		// 	$table->string('funded_by')->default(0)->comment('by-odas , by-mtls');
-		// 	$table->json('flat_rates')->nullable();
-		// 	$table->json('decrease_rates')->nullable();
-		// 	$table->json('loan_amounts')->nullable();
-		// 	$table->json('monthly_loan_amounts')->nullable();
-		// 	$table->studyFields();
-        //     $table->timestamps();
-        // });
+        Schema::connection(NON_BANKING_SERVICE_CONNECTION_NAME)->create('consumerfinance_product_sales_projects', function (Blueprint $table) {
+            $table->id();
+			$table->unsignedBigInteger('consumerfinance_product_id');
+			$table->unsignedBigInteger('tenor')->default(0);
+			$table->decimal('avg_amount',14,2)->default(0);
+			$table->json('monthly_amounts')->nullable();
+			$table->string('funded_by')->default(0)->comment('by-odas , by-mtls');
+			$table->json('flat_rates')->nullable();
+			$table->json('decrease_rates')->nullable();
+			$table->json('loan_amounts')->nullable();
+			$table->json('monthly_loan_amounts')->nullable();
+			$table->studyFields();
+            $table->timestamps();
+        });
     }
 
     /**
